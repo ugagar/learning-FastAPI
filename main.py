82c8383044cf47090d802e0baf5bd9b0f2d08fd5
Danil Ugarov <ugarovd06@gmail.com>
2025-07-03 01:09:49 +0300

    Add /items/0 endpoint. Check 'order matters'

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ async def root():
 async def foo():
     return "Bye, World!"
 
+@app.get("/items/0")
+async def read_zero_item():
+    return {"item_id": "This your item - 0"}
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
