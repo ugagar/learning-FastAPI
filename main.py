da2aa179fa1980ffc059ab4dc05d632711bd5ba9
Danil Ugarov <ugarovd06@gmail.com>
2025-07-04 09:22:24 +0300

    Fix pydantic depricated model method dict()

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def create_item(item: Item):
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+    result = {"item_id": item_id, **item.model_dump()}
     if q:
         result.update({"q": q})
     return result
