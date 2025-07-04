e8caa61ace1419fd40ca33c0710c5491887d9625
Danil Ugarov <ugarovd06@gmail.com>
2025-07-04 09:18:53 +0300

    Add /items/{item_id} endpoint with path/query params and request body

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ async def foo():
 @app.post("/items/")
 def create_item(item: Item):
     return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
